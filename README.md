0e8f0adc72dfec8a71fc80b8d75e1934b134bcbf
Zerui An <anzerui@126.com>
2022-04-05 20:46:55 -0500

    Add license badge and headers

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # WebFPGA-GUI
 
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 [![Windows Build](https://github.com/JerryAZR/WebFPGA-GUI/actions/workflows/win64-build.yml/badge.svg)](https://github.com/JerryAZR/WebFPGA-GUI/actions/workflows/win64-build.yml)
 
 Notes:
--- src/flash.py
+++ src/flash.py
@@ -1,3 +1,27 @@
+"""
+This file is part of the WebFPGA-GUI program
+(https://github.com/JerryAZR/WebFPGA-GUI).
+
+Copyright (c) 2022, Zerui An <anzerui@126.com / jerryazr@gmail.com>.
+
+This program is free software: you can redistribute it and/or modify  
+it under the terms of the GNU General Public License as published by  
+the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+The WebFPGA-GUI program is based on the WebFPGA CLI tool developed
+by the WebFPGA Team (https://github.com/webfpga/cli). The Copyright
+of the original program belongs to Auburn Ventures, LLC and is
+covered by the MIT License (https://opensource.org/licenses/MIT).
+"""
+
 from kivy.lang import Builder
 from kivymd.uix.gridlayout import MDGridLayout
 from popups import SynthPopup
